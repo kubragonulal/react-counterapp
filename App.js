c0fa36ea817bfdcc379645e04e18832d13d19651
kubragonulal <kubragonulal@gmail.com>
2023-10-16 16:55:56 +0300

    Counter added to the application
    Signed-off-by: kubragonulal <kubragonulal@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {
+  Text, 
+  SafeAreaView, 
+  StyleSheet, 
+  Image, 
+  View, 
+  Button,
+  TouchableOpacity
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+function MyCustomButton(props){
+  //props.title
+  return(
+  <TouchableOpacity style = {[styles.button, props.style]} 
+      onPress={props.onPress}>
+    <Text style = {styles.buttonText}>{props.title}</Text>
+  </TouchableOpacity>
   );
 }
 
+function App(){
+
+const [count, setCount] = useState(0);
+
+const handleIncrement = () => {
+  setCount(count + 1);
+};
+const handleDecrement = () => {
+  setCount(count - 1);
+
+};
+
+  return (
+   
+  <SafeAreaView>
+     <View style={styles.container}>
+    <Image 
+    source={{uri: "https://i.pinimg.com/originals/1b/ab/ba/1babba304aeae6d32624208602745fd2.jpg"}}
+    style={styles.image}/>
+  <Text style={styles.text}>Conter: {count}</Text>
+  <Text style={styles.subtitle}>
+    Click buttons to change the counter.
+    </Text>
+    <View style = {styles.buttonContainer}>
+    <MyCustomButton title = "Increment +" onPress={handleIncrement}/>
+    <MyCustomButton 
+    title = "Decrement -" 
+    style = {{backgroundColor:"pink", marginStart:16}} 
+    onPress={handleDecrement}
+    />
+    </View>
+  </View>
+  </SafeAreaView>
+  
+  ); 
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    margin:16,
   },
+  text:{
+    fontSize:30,
+    marginTop:16,
+  },
+  subtitle:{
+    fontSize:20,
+    color:"gray",
+    marginTop:4,
+  },
+  image:{
+    height:180,
+    borderRadius:12,
+  },
+  buttonContainer:{
+    marginTop:16,
+    flexDirection:"row"
+  },
+  button:{
+    flex: 1,
+    backgroundColor:"lightblue",
+    padding:12,
+    borderRadius:12,
+    alignItems:"center",
+  },
+  buttonText:{
+    color:"black",
+    fontSize:20,
+    textAlign:"center",
+  }
 });
+export default App;
